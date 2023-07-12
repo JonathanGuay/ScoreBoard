9d64714ae55500c066140aacabb798834a6878d0
JonathanGuay <116024586+JonathanGuay@users.noreply.github.com>
2023-07-12 15:32:47 -0400

    finit validation de joueur et jeu

--- ScoreBoard/Models/Jeu.cs
+++ ScoreBoard/Models/Jeu.cs
@@ -7,25 +7,27 @@ namespace ScoreBoard.Models
         [Required(ErrorMessage = "L'id est obligatoire")]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [Required(ErrorMessage = "Le nom est obligatoire")]
         public string Nom { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [Required(ErrorMessage = "La date de sortie est obligatoire")]
         public DateTime DateSortie { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [Required(ErrorMessage = "La description est obligatoire")]
         public string Description { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [Required(ErrorMessage = "L'id du joueur est obligatoire")]
         public int JoueurId { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [Required(ErrorMessage = "Le joueur est obligatoire")]
         public Joueur Joueur { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [RegularExpression(@"^{0-100}$",
+        ErrorMessage = "Le score doit être compris entre de 0 et 100")]
+        [Required(ErrorMessage = "Le score du joueur est obligatoire")]
         public int ScoreJoueur { get; set; }
 
-        [Required(ErrorMessage = "L'id est obligatoire")]
+        [Required(ErrorMessage = "La date d'enregistrement est obligatoire")]
         public DateTime DateEnregistrement { get; set; }
     }
 }
