7c9a05bd90785bae4c5a68ddb3aea333288fcaac
JonathanGuay <116024586+JonathanGuay@users.noreply.github.com>
2023-07-12 15:41:19 -0400

    complétation de validation de joueur et jeu

---
+++ ScoreBoard/Models/dbJoueurRep.cs
@@ -0,0 +1,22 @@
+﻿namespace ScoreBoard.Models
+{
+    public class dbJoueurRep : IJoueurRepository
+    {
+        public List<Joueur> ListeJoueurs { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Joueur? GetJoueur(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modifier(Joueur joueur)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Supprimer(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
